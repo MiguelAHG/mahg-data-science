d40462b8f973335e06f4100f62e4dd690e137c27
Miguel Antonio H. Germar <migs.germar@gmail.com>
2022-01-14 14:52:48 +0800

    Update skfcv module and linear regression proj

--- _notebooks/custom_modules/stratified_kfcv.py
+++ _notebooks/custom_modules/stratified_kfcv.py
@@ -4,87 +4,88 @@ author: Miguel Antonio H. Germar"""
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 
-def stratify_continuous(n_folds, data, target_col, fold_col = "fold"):
+def stratify_continuous(n_folds, y, random_state = 1):
     """Stratify a dataset on a continuous target.
 
-n_folds: Integer of folds to form.
-data: DataFrame containing at least the target column.
-target_col: String representing the target.
-fold_col: String, the name of the new column that will be added to the data.
+    n_folds: integer of folds to form.
+    y: Series containing target values.
+    random_state: integer used as random seed, default 1.
 
-Returns: DataFrame"""
+    Returns: Series of integers representing fold numbers."""
 
     if n_folds < 2 or n_folds > 10:
         raise ValueError("Please select a number of folds from 2 to 10.")
-
-    if fold_col in data.columns:
-        raise IndexError(f"{fold_col} is already in the columns.")
     
     fold_nums = list(range(n_folds))
-    data = (
-        data
-        # Shuffle before sorting so that observations with the same target value are ordered randomly.
-        .sample(frac = 1, random_state = 1)
-        .sort_values(target_col, ascending = True)
-        .reset_index(drop = True)
+
+    # DataFrame where "index" column contains the original indices
+    df = pd.DataFrame(
+        y
+        # Shuffle before ranking so that cars with the same price are ordered randomly.
+        .sample(frac = 1, random_state = random_state, ignore_index = False)
     )
-    data[fold_col] = 0
+
+    # This column gives a rank to each value in y. 0 is the rank of the lowest value.
+    # Ties are broken according to order of appearance.
+    df["rank"] = df[y.name].rank(method = "first") - 1
+
+    df["fold"] = 0
 
     for f in fold_nums[1:]:
         # start at f, then increment by n_folds
-        indices = list(range(f, data.shape[0], n_folds))
-        data.loc[indices, fold_col] = f
+        indices = list(range(f, df.shape[0], n_folds))
+        df.loc[df["rank"].isin(indices), "fold"] = f
 
-    return data
+    # Revert df to original order of indices
+    df = df.reindex(index = y.index)
 
-def split_folds(data, feature_cols, target_col, test_fold, fold_col = "fold"):
+    # A series that indicates the fold number of each observation according to its original position in y
+    fold_series = df["fold"].copy()
+
+    return fold_series
+
+def split_folds(X, y, fold_series, test_fold):
     """Take a dataset whose observations have been grouped into folds,
     then perform a train-test split.
     
-data: DataFrame with feature and target data.
-feature_cols: list of str, the names of the feature columns.
-target_col: str, the name of the target column.
-test_fold: int, the numeric label of the fold that will be considered as the testing set. This can range from 0 to n_folds - 1.
-fold_col: str, the name of the column that indicates the fold of each observation.
+    X, y: feature and target DataFrames.
+    fold_series: Series containing the fold numbers of the observations.
+    test_fold: Integer, the fold number that will be used as the test fold.
 
-Returns: tuple of DataFrame"""
+    Returns: tuple of four DataFrames"""
 
-    if data[fold_col].dtype != "int64":
-        raise AttributeError("The 'fold' column is not an int64 column.")
+    if fold_series.dtype != "int64":
+        raise AttributeError("The fold list does not purely contain integers.")
 
-    test = data[data[fold_col] == test_fold]
-    train = data[data[fold_col] != test_fold]
+    test_mask = (fold_series == test_fold)
 
-    X_train = train[feature_cols].copy()
-    y_train = train[target_col].copy()
+    X_train = X.loc[~test_mask].copy()
+    y_train = y.loc[~test_mask].copy()
 
-    X_test = test[feature_cols].copy()
-    y_test = test[target_col].copy()
+    X_test = X.loc[test_mask].copy()
+    y_test = y.loc[test_mask].copy()
 
     return X_train, X_test, y_train, y_test
 
-def stratified_kfcv(data, feature_cols, target_col, regression_model, fold_col = "fold"):
+def stratified_kfcv(X, y, fold_series, regression_model):
     """Conduct k-fold cross-validation on a stratified dataset.
 
-data: DataFrame with feature and target data.
-feature_cols: list of str, the names of the feature columns.
-target_col: str, the name of the target column.
-regression_model: instance of a scikit-learn class for regression. Must have fit() and predict() methods.
-fold_col: str, the name of the column that indicates the fold of each observation.
+    X, y: feature and target DataFrames.
+    fold_series: Series that indicates the fold number of each observation.
+    regression_model: instance of a scikit-learn class for regression. Must have fit() and predict() methods.
 
-returns: list of float"""
+    returns: list of floats representing MSE values"""
 
-    fold_nums = data[fold_col].unique()
+    fold_nums = fold_series.unique()
 
     mse_lst = []
 
     for f in fold_nums:
         X_train, X_test, y_train, y_test = split_folds(
-            data,
-            feature_cols = feature_cols,
-            target_col = target_col,
+            X = X,
+            y = y,
             test_fold = f,
-            fold_col = fold_col,
+            fold_series = fold_series,
         )
         
         regression_model.fit(X_train, y_train)
